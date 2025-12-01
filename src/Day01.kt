fd28a0b9da0318ce932a8c302f29cd68986a58bb
jason <jason@tavernier.nl>
2025-12-01 06:30:14 +0100

    day1 - iteratively going over number by number could've been done, its too early though

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,53 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var cur = 50
+        var counter = 0
+        for(line in input) {
+            val direction = line[0]
+            val num = line.drop(1).toInt()
+            if (direction == 'L'){
+                cur -= num
+            }
+            else if (direction == 'R'){
+                cur += num
+            }
+            while(cur < 0){
+                cur += 100
+            }
+            cur %= 100
+
+            if(cur == 0){
+                counter += 1
+            }
+        }
+        return counter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var cur = 50
+        var counter = 0
+        for(line in input) {
+            val direction = line[0]
+            val num = line.drop(1).toInt()
+            for (i in 1..num){
+                if(direction == 'L'){
+                    cur--
+                }
+                else{
+                    cur++
+                }
+                cur %= 100
+                if (cur == 0){
+                    counter += 1
+                }
+            }
+        }
+        return counter
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
